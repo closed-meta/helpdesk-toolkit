bb1f18fb39475e816b179620437aa1d396ab3211
closed-meta <166439312+closed-meta@users.noreply.github.com>
2025-02-19 17:19:19 -0500

    Added help info to Copy-Ticket.ps1.

--- hdtk/internal-functions/Copy-Ticket.ps1
+++ hdtk/internal-functions/Copy-Ticket.ps1
@@ -1,4 +1,16 @@
-# [TODO] Add manual.
+<#
+  .SYNOPSIS
+    Helps the user quickly copy the ticket subject, body and fulfillment comment to their clipboard one at a time.
+
+  .PARAMETER Subject
+    Represents the ticket subject to be copied to the clipboard.
+
+  .PARAMETER Body
+    Represents the ticket body to be copied to the clipboard.
+
+  .PARAMETER Fulfillment
+    Represents the ticket fulfillment comment to be copied to the clipboard.
+#>
 
 param (
   [string]$Subject,
